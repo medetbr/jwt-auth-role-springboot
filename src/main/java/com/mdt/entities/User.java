59156024791743311ad57e7cf95093ddfba434be
medetbr <berimen35@gmail.com>
2025-07-05 10:01:07 +0300

    first commit

---
+++ src/main/java/com/mdt/entities/User.java
@@ -0,0 +1,19 @@
+package com.mdt.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // örn: ROLE_USER
+}
